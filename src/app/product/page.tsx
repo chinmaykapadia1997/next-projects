b7e4a1894d19855a57c12d5a6d55d7de4e7d99f9
chinmaykapadia1997 <chinmaykapadia1@gmail.com>
2024-04-24 15:40:43 +0530

    final push

--- src/app/product/page.tsx
+++ src/app/product/page.tsx
@@ -1,39 +1,39 @@
-// API can hit in 2 ways 1.1 server side client (useEffect in react)
-// for Server side :-> API hit using next
+// // API can hit in 2 ways 1.1 server side client (useEffect in react)
+// // for Server side :-> API hit using next
 
-import Link from "../../../node_modules/next/link";
-import ProductDelete from "./delete";
-//SSR
-async function getProducts() {
-    const res = await fetch(`http://localhost:7001/product`, { cache: 'no-store' })
-    if (!res.ok) {
-        throw new Error('Failed to fetch data')
-    }
-    return res.json()
-}
+// import Link from "../../../node_modules/next/link";
+// import ProductDelete from "./delete";
+// //SSR
+// async function getProducts() {
+//     const res = await fetch(`http://localhost:7001/product`, { cache: 'no-store' })
+//     if (!res.ok) {
+//         throw new Error('Failed to fetch data')
+//     }
+//     return res.json()
+// }
 
 
-export default async function page(){
-
-  
-    const products = await getProducts()
+// export default async function page(){
 
   
-    return(
+//     const products = await getProducts()
 
-        <div className="w-1/2 grid gap-4 mx-auto p-8">
-            <Link href={'/product/add'} className="bg-green-700 text-gray-100 w-64 p-2 text-center">Add Product</Link>
-            {products.map((product: any, i: number) => {
-                return (
-                    <div className="grid grid-cols-7 gap-2">
-                        <div className="col-span-5">{i+1}. {product.name}</div>
-                        <Link href={`/product/${product.id}`} className="font-bold border p-1 bg-red-700 text-gray-100">Edit</Link>
-                      <ProductDelete id={product.id}/>  
-                    </div>
+  
+//     return(
 
-                )
-            })}
-        </div>
+//         <div className="w-1/2 grid gap-4 mx-auto p-8">
+//             <Link href={'/product/add'} className="bg-green-700 text-gray-100 w-64 p-2 text-center">Add Product</Link>
+//             {products.map((product: any, i: number) => {
+//                 return (
+//                     <div className="grid grid-cols-7 gap-2">
+//                         <div className="col-span-5">{i+1}. {product.name}</div>
+//                         <Link href={`/product/${product.id}`} className="font-bold border p-1 bg-red-700 text-gray-100">Edit</Link>
+//                       <ProductDelete id={product.id}/>  
+//                     </div>
 
-    )
-}
+//                 )
+//             })}
+//         </div>
+
+//     )
+// }
